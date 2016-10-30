5faabb1c16fcf96c0645b7d42bf9ab495f8e8406
Kai Hsu <kaiihsu@gmail.com>
2016-10-30 01:07:45 -0500

    saving crypted login info to mongo

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-
+var mongoose = require('mongoose');
+var reqall = require('require-all');
 var app = express();
+
+reqall({
+    dirname: __dirname + '/models',
+    recursive: true
+});
+mongoose.connect('mongodb://public:public1@ds031257.mlab.com:31257/kaidb');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,13 +27,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', objectToArray(reqall({
+    dirname: __dirname + '/routes',
+    recursive: true
+})));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -35,24 +44,62 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
 module.exports = app;
+
+/*function getMiddleWare(folders) {
+    var middleWareArray = [];
+
+    folders.forEach(function(folder) {
+        var files = fs.readdirSync(path.join(__dirname, folder));
+
+        files.forEach(function(file) {
+            var filepath = path.join(__dirname, folder, file);
+            if (fs.statSync(filepath).isDirectory()) {
+                middleWareArray.concat(getMiddleWare([path.join(folder, file)]));
+            } else {
+                console.log(filepath);
+                middleWareArray.push(require(filepath));
+            }
+        });
+    });
+
+    return middleWareArray;
+}
+*/
+
+function objectToArray(obj) {
+    var result = [];
+
+    for (var key in obj) {
+        if (typeof obj[key] === 'object') {
+            console.log('entered');
+            var resArr = objectToArray(obj[key]);
+            resArr.forEach(function(item) {
+                result.push(item);
+            });
+        } else {
+            result.push(obj[key]);
+        }
+    }
+    return result;
+}
---
+++ models/mgSchema.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+
+var userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    hash: String,
+    salt: String,
+    creation: { type: Date, default: Date.now },
+    history: [{ type: mongoose.Schema.Types.ObjectId, ref: 'History' }]
+});
+
+userSchema.methods.crpytPassword = function(pw) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(pw, this.salt, 1000, 64).toString('hex');
+};
+
+userSchema.methods.isValidPassword = function(pw) {
+    var hash = crypto.pbkdf2Sync(pw, this.salt, 1000, 64).toString('hex');
+    return this.hash === hash;
+};
+
+var historySchema = new mongoose.Schema({
+    wins: { type: Number, default: 0 },
+    losses: { type: Number, default: 0 },
+    draws: { type: Number, default: 0 },
+    date: { type: Date, default: Date.now },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+mongoose.model('User', userSchema);
+
+mongoose.model('History', historySchema);
